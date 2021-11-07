3fb6d4e83d66184188d759f9e598072798771593
bori00 <fazakasbori@gmail.com>
2021-11-07 16:56:53 +0200

    implement ex1

--- src/Model/PersonalDetails.elm
+++ src/Model/PersonalDetails.elm
@@ -1,7 +1,7 @@
 module Model.PersonalDetails exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, classList, id)
+import Html.Attributes exposing (class, classList, href, id)
 
 
 type alias DetailWithName =
@@ -20,5 +20,24 @@ type alias PersonalDetails =
 
 view : PersonalDetails -> Html msg
 view details =
-    div [] []
-    -- Debug.todo "Implement the Model.PersonalDetails.view function"
+    div [] [
+        h1 [id "name"] [text details.name],
+        em [id "intro"] [text details.intro],
+        div [id "contactsDiv"] [
+            h3 [id "contactsDivTitle"] [text "Contact"],
+            ul [] <|
+                List.map
+                    (\contactDetailWithName -> li [class "contact-detail"]
+                        [text (contactDetailWithName.name ++ ": " ++ contactDetailWithName.detail)])
+                details.contacts
+        ],
+        div [id "socialMediaDiv"] [
+            h3 [id "socialMediaDivTitle"] [text "Social Media"],
+            ul [] <|
+                List.map
+                    (\socialLinkWithName -> li [class "social link"][
+                            a [href socialLinkWithName.detail] [text socialLinkWithName.name]
+                        ])
+                details.socials
+        ]
+    ]
